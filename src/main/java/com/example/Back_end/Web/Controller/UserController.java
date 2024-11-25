6cb1ecf4ed59769225f6ec46926bf9156b7fbac5
Cauã Barros Moreira <cauabneo@gmail.com>
2024-11-25 14:52:40 -0300

    coloquei o cors pra nao dar erro

--- src/main/java/com/example/Back_end/Web/Controller/UserController.java
+++ src/main/java/com/example/Back_end/Web/Controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/user")
 public class UserController {
     @Autowired
